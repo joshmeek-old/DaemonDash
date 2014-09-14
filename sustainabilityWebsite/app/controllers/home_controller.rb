e8efc8753697172ef3f4b89cd8a5bdaf0dca798a
Lucas Cheng <lucas.cheng96@gmail.com>
2014-09-14 05:30:23 -0400

    Fixed that controller

--- sustainabilityWebsite/app/controllers/home_controller.rb
+++ sustainabilityWebsite/app/controllers/home_controller.rb
@@ -7,13 +7,7 @@ class HomeController < ApplicationController
     @homes=Home.new
   end
   def index
-    @homes=Home.new
-    csv_text = File.read(Rails.root + "lib/assets/zip_code_database.csv")
-    csv = CSV.parse(csv_text, :headers => true)
-    csv.each do |row|
-      Home.create!(row.to_hash)
-    end
-        @homes=Home.all
+    
   end
       
   def search
