d7e2e248841782cb31d8d92535ba95d5eb5c45b6
Josh Meek <joshmeek18@gmail.com>
2014-09-14 05:39:11 -0400

    FIxed more issues

--- sustainabilityWebsite/db/schema.rb
+++ sustainabilityWebsite/db/schema.rb
@@ -12,12 +12,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20140914010736) do
-  
+
   create_table "homes", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "LocationString"
-  
   end
 
   create_table "locations", force: true do |t|
