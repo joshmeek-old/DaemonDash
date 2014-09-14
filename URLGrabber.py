0dbcd4a2ce7891662aa7572e801e5036a2c5d461
DustinBrash <xrunner23@gmail.com>
2014-09-14 00:08:37 -0400

    Added air quality method, returns state and quality as a tuple, takes int zipcode and string date(yyyy-mm-dd) as parameters

--- URLGrabber.py
+++ URLGrabber.py
@@ -10,4 +10,13 @@ def uvIndex(zipcode):
     index = soup.find('img', {'alt' : re.compile('UVI')})
     print index['alt'][-1]
 
+def aqIndex(zipcode, date):
+    #zipcode, date = 20852, '2014-09-13'
+    inputs = {'zipCode' : zipcode, 'date' : date, 'distance' : 25, 'API_KEY' : '9E84FE3D-6C7C-4238-8C89-5B8098D94DC6'}
+    r = requests.get('http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json', params=inputs)
+    aqi = r.json()
+    state = aqi[0]['StateCode']
+    quality = aqi[0]['Category']['Name']
+    return state, quality
+
     
